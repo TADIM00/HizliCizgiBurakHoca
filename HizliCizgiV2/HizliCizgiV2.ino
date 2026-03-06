3b3b7d3db26e32099c60d83175b3b44561322d47
GOSBTJYRobotik <gosbtadimjale@gmail.com>
2026-03-06 15:57:50 +0300

    HizliCizgiV3
    Kalibrasyonda iyleştirmeler yapıldı.
    düz yolda(20mm kalınlık)tam gaz gidiyor
    40 derece 30derece viraj dönebiliyor.

--- HizliCizgiV2/HizliCizgiV2.ino
+++ HizliCizgiV2/HizliCizgiV2.ino
@@ -1,124 +1,102 @@
 #include <QTRSensors.h>
 
-// --- 1. TANIMLAMALAR ---
-#define engel_sensoru A0    // MZ-80 Sinyal pini
-// int sensor = 10;         // Bu pin QTR ile çakıştığı için iptal edildi, donanımsal çakışma drift sebebidir.
+// --- 1. PINLER ---
+#define engel_sensoru A0    
+#define sag_yon 13          
+#define sag_pwm 11          
+#define sol_yon 12          
+#define sol_pwm 3           
 
-// Taban Hızları (Profast Motorlar ve 11.1V Pil için ideal başlangıç)
-int sagtabanhiz = 100;     
-int soltabanhiz = 100;     
-
-// Motor Pinleri (Ardumoto Shield)
-#define sagmotoryon 13      
-#define sagmotorpwmpin 11   
-#define solmotoryon 12      
-#define solmotorpwmpin 3    
+// --- 2. AGRESİF HIZ AYARLARI ---
+int sag_taban_hiz = 220;    // Düzlükte uçuş modu (185'ten 220'ye çıktı)
+int sol_taban_hiz = 220;     
+float yavaslama_katsayisi = 0.02; // Daha az fren, daha çok hız
 
 QTRSensors qtr; 
 const uint8_t SensorSayisi = 8;
 unsigned int sensorValues[SensorSayisi]; 
 
-// PID ve Değişkenler
 int sonhata = 0;
-float Kp = 0.25;  // Çizgi takip gayreti (Düşükse robot tepki vermez)
-float Kd = 2.0;   // Savrulma (drift) önleyici
-int sagmotorpwm = 0;
-int solmotorpwm = 0;
-int zemin = 0;    // 0: Beyaz Zemin - Siyah Çizgi, 1: Siyah Zemin - Beyaz Çizgi
+float Kp = 0.55;  // Hız arttığı için çizgiyi daha sert tutmalı
+float Kd = 6.0;   // Yüksek hızda kafa atmasını engellemek için Kd artırıldı
 
 void setup() {
   delay(2000); 
-  
-  // Pin Modları
   pinMode(engel_sensoru, INPUT); 
-  pinMode(sagmotoryon, OUTPUT);
-  pinMode(sagmotorpwmpin, OUTPUT);
-  pinMode(solmotoryon, OUTPUT);
-  pinMode(solmotorpwmpin, OUTPUT);
+  pinMode(sag_yon, OUTPUT);
+  pinMode(sag_pwm, OUTPUT);
+  pinMode(sol_yon, OUTPUT);
+  pinMode(sol_pwm, OUTPUT);
 
-  // QTR Sensör Ayarları (Pinlerin tam sırası)
   qtr.setTypeRC();
   qtr.setSensorPins((const uint8_t[]){2, 4, 5, 6, 7, 8, 9, 10}, SensorSayisi);
-  qtr.setEmitterPin(255); 
-
-  // --- KALİBRASYON (HASSAS VE SENİN MOTORLARINA UYGUN) ---
+  
+  // --- GÜÇLENDİRİLMİŞ KALİBRASYON ---
   digitalWrite(LED_BUILTIN, HIGH); 
-  for (uint16_t i = 0; i < 200; i++) {
+  for (uint16_t i = 0; i < 250; i++) {
     qtr.calibrate(); 
-    if (i < 150) {
-       // Kalibrasyonda tam tarama için motor gücü 55
+    if (i < 200) {
+       // 55 PWM: Motorun dişliyi rahat döndürdüğü ama fırlamadığı değer
        if ((i / 15) % 2 == 0) motorkontrol(-55, 55); 
        else motorkontrol(55, -55);
-    } else {
-       frenle();
-    }
+    } else { frenle(); }
     delay(2); 
+    if (i % 8 == 0) motorkontrol(0, 0); // Daha az kesinti, daha akıcı tarama
   }
   digitalWrite(LED_BUILTIN, LOW); 
-  
   frenle();
   delay(1000);
-  Serial.begin(9600);
 }
 
 void loop() {
-  // --- 1. MZ-80 ENGEL KONTROLÜ ---
+  // ENGEL KONTROLÜ
   if (digitalRead(engel_sensoru) == LOW) { 
     frenle();
     while(digitalRead(engel_sensoru) == LOW); 
-    delay(100); 
   }
 
-  // --- 2. ÇİZGİ OKUMA VE ZEMİN ALGILAMA ---
+  // ÇİZGİ OKUMA
   uint16_t position = qtr.readLineBlack(sensorValues);  
-  
-  // Senin orijinal kodundaki zemin ayrımı mantığı
-  if (sensorValues[0] < 200 && sensorValues[7] < 200) {
-    zemin = 0; // Beyaz zemin
-  } else if (sensorValues[0] > 600 && sensorValues[7] > 600) {
-    zemin = 1; // Siyah zemin
-  }
-
-  // --- 3. PID HESAPLAMA ---
   int hata = position - 3500; 
-  int duzeltmehizi = (Kp * hata) + (Kd * (hata - sonhata));
+
+  // PID HESAPLAMA
+  int duzeltme = (Kp * hata) + (Kd * (hata - sonhata));
   sonhata = hata;
 
-  // --- 4. MOTOR HIZLARI (DRIFT ÖNLEYİCİ TERS YÖN KONTROLÜ) ---
-  // Çizgi sağa kaçarsa (hata > 0), Sol motor hızlanmalı, Sağ motor yavaşlamalı.
-  sagmotorpwm = sagtabanhiz - duzeltmehizi;
-  solmotorpwm = soltabanhiz + duzeltmehizi;
+  // --- TURBO HIZ MANTIĞI ---
+  int aktif_hiz = sag_taban_hiz; 
 
-  // Hız Sınırlandırma (Profast motorlar için genişletilmiş aralık)
-  sagmotorpwm = constrain(sagmotorpwm, -120, 200);
-  solmotorpwm = constrain(solmotorpwm, -120, 200);
-
-  motorkontrol(sagmotorpwm, solmotorpwm); 
-}
-
-// --- 5. MOTOR KONTROL FONKSİYONLARI (SENİN ASIL GÜVENDİĞİN BLOK) ---
-void motorkontrol(int sagpwm, int solpwm) {
-  // Sağ Motor
-  if (sagpwm <= 0) {
-    digitalWrite(sagmotoryon, LOW);
-    analogWrite(sagmotorpwmpin, abs(sagpwm));
-  } else {
-    digitalWrite(sagmotoryon, HIGH);
-    analogWrite(sagmotorpwmpin, sagpwm);
+  // Virajda hızı sadece hata çok büyükse (1500+) ve daha az miktarda kırıyoruz
+  if (abs(hata) > 1500) {
+    aktif_hiz = sag_taban_hiz - (abs(hata) * yavaslama_katsayisi);
   }
 
-  // Sol Motor
-  if (solpwm <= 0) {
-    digitalWrite(solmotoryon, LOW);
-    analogWrite(solmotorpwmpin, abs(solpwm));
+  int sag_motor_pwm = aktif_hiz - duzeltme;
+  int sol_motor_pwm = aktif_hiz + duzeltme;
+
+  // MAX GÜÇ (255 tam kapasite)
+  sag_motor_pwm = constrain(sag_motor_pwm, -255, 255);
+  sol_motor_pwm = constrain(sol_motor_pwm, -255, 255);
+
+  motorkontrol(sag_motor_pwm, sol_motor_pwm); 
+}
+
+void motorkontrol(int sagpwm, int solpwm) {
+  if (sagpwm <= 0) {
+    digitalWrite(sag_yon, LOW);
+    analogWrite(sag_pwm, min(abs(sagpwm), 255));
   } else {
-    digitalWrite(solmotoryon, HIGH);
-    analogWrite(solmotorpwmpin, solpwm);
+    digitalWrite(sag_yon, HIGH);
+    analogWrite(sag_pwm, min(sagpwm, 255));
+  }
+
+  if (solpwm <= 0) {
+    digitalWrite(sol_yon, LOW);
+    analogWrite(sol_pwm, min(abs(solpwm), 255));
+  } else {
+    digitalWrite(sol_yon, HIGH);
+    analogWrite(sol_pwm, min(solpwm, 255));
   }
 }
 
 void frenle() { motorkontrol(0, 0); }
-
-// Yardımcı Fonksiyonlar (Kalibrasyon ve Manevra için)
-void tamSagaDon() { motorkontrol(-80, 80); }
-void tamSolaDon() { motorkontrol(80, -80); }
